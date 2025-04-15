a2fa1988fa5574c85e21057d021a85cee9e1890f
NicoMaker <nicola.marano02@gmail.com>
2025-04-15 15:20:37 +0200

    Update scripts to improve functionality and fix errors in first_scripts.sh and myscript1.sh

--- Lezioni/5_Lezione/scripts/first_scripts.sh
+++ Lezioni/5_Lezione/scripts/first_scripts.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# Crea la directory 'dir1' se non esiste già
 mkdir -p dir1
+
+# Stampa una stringa a schermo
 echo "ITS ALTO ADRIATCO"
 
+# Elenca i file presenti nella directory 'dir1' con dettagli
 ls -l dir1
+
+# Cerca di leggere un file che non è stato creato, quindi darà errore
 cat dir1/comunicazioni.txt
--- Lezioni/5_Lezione/scripts/myscript1.sh
+++ Lezioni/5_Lezione/scripts/myscript1.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/bin/python3
 
-import sys
-ptint(sys.version)
+
+import sys  # Importa il modulo sys per accedere alle funzionalità del sistema operativo
+print(sys.version)  # Questo stampa la versione di Python in uso
